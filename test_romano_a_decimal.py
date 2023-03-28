3b7111e000957507986b51dc936077afe3049d7b
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2023-03-28 16:21:24 -0300

    SSH

--- test_romano_a_decimal.py
+++ test_romano_a_decimal.py
@@ -3,7 +3,7 @@ import unittest
 
 def roman_to_decimal(romano):
     d_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C' : 100, 'D': 500, 'M': 1000}
-    d_decimal= [d_num[char] for char in romano] #char=variable temporal para iterar
+    d_decimal= [d_num[n] for n in romano] #n=variable temporal para iterar
     total = 0
     for i in range(len(d_decimal)):
         if i == len(d_decimal) -1 or d_decimal[i] >= d_decimal[i+1]:
